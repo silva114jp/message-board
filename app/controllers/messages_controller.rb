34b6633389ea91cd9e677471b7cfa93188497992
silva114jp <silva114jp@gmail.com>
2016-01-13 13:42:05 +0000

    exam no.2 finish

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,15 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice:'メッセージを保存しました'
+
+    if @message.save
+      redirect_to root_path , notice:'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった場合
+      @message = Message.all
+      flash.now[:alert] = 'メッセージの保存に失敗しました'
+      render 'index'
+    end
   end
   
   private
